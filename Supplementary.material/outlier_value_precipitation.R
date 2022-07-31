85a76f3f48a188afc20150f803a1edde84d8c498
fritte2 <ritter.francois@gmail.com>
2022-07-31 14:01:17 +0200

    Update outlier_value_precipitation.R

--- Supplementary.material/outlier_value_precipitation.R
+++ Supplementary.material/outlier_value_precipitation.R
@@ -6,30 +6,30 @@ setwd(path.precipitation)
 library(data.table)
 stations <- list.files(pattern='*.csv$')
 
-all.a <- c()
+all.lambda <- c()
 for(i in 1:length(stations))
 {
   print(i)
-  plop <- fread(stations[i])
+  station.i <- fread(stations[i])
   
-  y.max <- max(unlist(plop[,prcp]),na.rm=T)*1.2 # events 20% above the maximum precipitation in the century are considered as impossible
+  y.max <- max(unlist(station.i[,prcp]),na.rm=T)*1.2 # events 20% above the maximum precipitation in the century are considered as impossible
   
-  if(nrow(plop) >= 365*100)
+  if(nrow(station.i) >= 365*100)
   {
     # randomly select 30 years
-  date.1 <- sample(unlist(plop[,date])[1:(nrow(plop)-365*30)],1)
+  date.1 <- sample(unlist(station.i[,date])[1:(nrow(station.i)-365*30)],1)
   date.2 <- date.1+30*365
   
   
-  y.max.30 <- max(unlist(plop[date.1 <= date & date <= date.2,prcp]),na.rm=T) # calculate the maximum
+  y.max.30 <- max(unlist(station.i[date.1 <= date & date <= date.2,prcp]),na.rm=T) # calculate the maximum
   
   if(y.max != 0 & y.max.30 != 0)
   {
-    all.a <- c(all.a,y.max/y.max.30)
+    all.lambda <- c(all.lambda,y.max/y.max.30)
   }
   }
 }
 
-print(paste0('acceptable outlier value for the precipitation : y.outlier = y.max.30years * ',round(mean(all.a),digits=1)))
+print(paste0('acceptable outlier value for the precipitation : y.outlier = y.max.30years * ',round(mean(all.lambda),digits=1)))
 
 
