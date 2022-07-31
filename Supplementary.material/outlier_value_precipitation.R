b0da44bf1aa8a37b32a7f6ab04aff2d0d6ef2dcf
fritte2 <ritter.francois@gmail.com>
2022-07-31 13:43:36 +0200

    value of 1.6 for the precipitation level

---
+++ Supplementary.material/outlier_value_precipitation.R
@@ -0,0 +1,35 @@
+# this script justifies why the outlier level for the precipitation is 1.6 * y.max
+
+path.precipitation <- "~/Science/ctbi_submission/residuals/IN_precipitation/"
+setwd(path.precipitation)
+
+library(data.table)
+stations <- list.files(pattern='*.csv$')
+
+all.a <- c()
+for(i in 1:length(stations))
+{
+  print(i)
+  plop <- fread(stations[i])
+  
+  y.max <- max(unlist(plop[,prcp]),na.rm=T)*1.2 # events 20% above the maximum precipitation in the century are considered as impossible
+  
+  if(nrow(plop) >= 365*100)
+  {
+    # randomly select 30 years
+  date.1 <- sample(unlist(plop[,date])[1:(nrow(plop)-365*30)],1)
+  date.2 <- date.1+30*365
+  
+  
+  y.max.30 <- max(unlist(plop[date.1 <= date & date <= date.2,prcp]),na.rm=T) # calculate the maximum
+  
+  if(y.max != 0 & y.max.30 != 0)
+  {
+    all.a <- c(all.a,y.max/y.max.30)
+  }
+  }
+}
+
+print(paste0('acceptable outlier value for the precipitation : y.outlier = y.max.30years * ',round(mean(all.a),digits=1)))
+
+
